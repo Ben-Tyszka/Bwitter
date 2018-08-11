eab8d3fafe8b2cdcd2eb340543fdc90882a1a040
Benjamin Tyszka <benjamintyszka@gmail.com>
2018-08-11 11:55:35 -0400

    resolve issue #2

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,50 @@ import * as React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
+import firebase from './bwitterFirebase'
+import AppPage from './pages/AppPage'
 import CreateAccountPage from './pages/CreateAccountPage'
 import HomePage from './pages/HomePage'
 import store from './store'
 
-class App extends React.Component {
+interface IState {
+  authorized: boolean
+  onAuthStateChangedUnsub: firebase.Unsubscribe | null
+}
+
+export default class App extends React.Component<{}, IState> {
+  state: IState = {
+    authorized: false,
+    onAuthStateChangedUnsub: null,
+  }
+  componentDidMount() {
+    const onAuthStateChange = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        authorized: user ? true : false
+      })
+    })
+
+    this.setState({
+      onAuthStateChangedUnsub: onAuthStateChange
+    })
+  }
+  componentWillUnmount() {
+    const { onAuthStateChangedUnsub } = this.state
+    if (onAuthStateChangedUnsub !== null) {
+      onAuthStateChangedUnsub()
+    }
+  }
   render() {
+    const { authorized } = this.state
     return (
       <Provider store={store}>
         <React.Fragment>
           <CssBaseline />
           <Router>
             <div>
-              <Route exact={true} path='/' component={HomePage} />
-              <Route exact={true} path='/register' component={CreateAccountPage} />
+              <Route exact={true} path='/' component={() => (authorized ? <AppPage /> : <HomePage />)} />
+              <Route path='/register' component={() => (authorized ? <Redirect to='/' /> : <CreateAccountPage />)} />
             </div>
           </Router>
         </React.Fragment>
@@ -24,5 +54,3 @@ class App extends React.Component {
     )
   }
 }
-
-export default App
